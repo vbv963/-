a3c8c899f8ff5c3972d55d2b8ca69c8b75056c7a
vbv963 <vbv963@gmail.com>
2023-10-13 23:54:39 +0900

    클리커 기능 구현

--- 중간_젤리 키우기/Assets/Scripts/GameManager.cs
+++ 중간_젤리 키우기/Assets/Scripts/GameManager.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level - 1];
+    }
 }
